75c147e6b2e9450a821a83d9b140b764670791a5
James Kennewell <ieatbedrock@gmail.com>
2020-10-21 09:19:38 +1030

    Major Change

--- MqttLightswitchcontrolv1.ino
+++ MqttLightswitchcontrolv1.ino
@@ -1,256 +1,32 @@
 #include <Wire.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-#include <I2CButton.h>
 #include <Adafruit_MCP23017.h>
 
 
 
-/* NETWORK SETTINGS */
-#define ENABLE_MAC_ADDRESS_ROM			true
-#define MAC_I2C_ADDR					0x50
-byte network_mac[] = {0x0F, 0x51, 0xAF, 0x24, 0x3E, 0x4E};
+/*--------------------------- CONFIGURATION ------------------------------*/
+// Configuration should be done in the included file:
+#include "config.h"
+
+
+
+/*--------------------------- SETUP -------------------------------------*/
 EthernetClient network_client;
 
+PubSubClient mqtt_client(network_client);
 
-/* MQTT SETTINGS */
-IPAddress mqtt_broker(XXX, XXX, XXX, XXX);
-PubSubClient mqtt(network_client);
-
-String mqtt_client = "Lcontrol-01";
-const char mqtt_user[] = "XXXX";
-const char mqtt_pass[] = "XXXX";
-unsigned long mqtt_timer = 0;
-
-
-/* Watchdog */
-int watchdog_pin = 3;
-bool watchdog_status = false;
-unsigned long watchdog_timmer = 0;
-
-
-/* Buttons */
 Adafruit_MCP23017 mcps[6];
-I2CButton buttons[96];
 
+bool watchdog_status = false;
+unsigned long watchdog_timer = 0;
+unsigned long mqtt_timer = 0;
+unsigned long heart_timer = 0;
+char mqtt_buffer[100];
+byte button_states[96] = {HIGH};
+unsigned long button_timers[96] = {0};
 
-
-/**
- * Initial setup
- *
- * @return void
- */
-void setup()
-{
-	Wire.begin();
-
-	if (ENABLE_MAC_ADDRESS_ROM == true)
-	{
-		network_mac[0] = readRegister(0xFA);
-		network_mac[1] = readRegister(0xFB);
-		network_mac[2] = readRegister(0xFC);
-		network_mac[3] = readRegister(0xFD);
-		network_mac[4] = readRegister(0xFE);
-		network_mac[5] = readRegister(0xFF);
-	}
-
-	// Wait for network connection
-	while (Ethernet.begin(network_mac) == 0)
-	{
-		delay(1000);
-	}
-
-	// Initializes the pseudo-random number generator
-	randomSeed(micros());
-
-	// Configure MQTT
-	mqtt.setServer(mqtt_broker, 1883);
-	mqtt.setCallback(mqttCallback);
-
-	// Setup pins and configure button
-	int id = 0;
-	for (int i = 0; i < 6; i++)
-	{
-		mcps[i].begin(i);
-
-		for (int p = 0; p < 16; p ++)
-		{
-			mcps[i].pinMode(p, INPUT);
-			buttons[id].configure(id, PULL_UP, onPress, onDblPress, onHold);
-			id ++;
-		}
-	}
-}
-
-
-/**
- * Main program loop
- *
- * @return void
- */
-void loop()
-{
-	// Keep network connection
-	Ethernet.maintain();
-
-	// Reconnect to MQTT server
-	if (!mqtt.connected())
-	{
-		if (!mqttReconnect())
-		{
-			// Exit early of loop
-			return;
-		}
-	}
-
-	// Check for incoming MQTT messages
-	mqtt.loop();
-
-	// Update each button's state
-	int id = 0;
-	for (int i = 0; i < 6; i++)
-	{
-		for (int p = 0; p < 16; p ++)
-		{
-			buttons[id].check(mcps[i].digitalRead(p));
-			id ++;
-		}
-	}
-
-	// Prevent arduino from resetting if everything is fine
-	resetWatchDog();
-}
-
-
-
-/**
- * MQTT reconnect
- * @return bool True on successful connection
- * @return bool False on failure to connect
- */
-bool mqttReconnect()
-{
-	// Watchdog
-	digitalWrite(watchdog_pin, LOW);
-	
-	// Too early to reconnect
-	if (!getTimer(mqtt_timer, 1000))
-	{
-		return false;
-	}
-
-	// Subscribe if connected
-	if (mqtt.connect(String(mqtt_client + String(random(0xffff), HEX)).c_str(), mqtt_user, mqtt_pass))
-	{
-		return true;
-	}
-
-	// Connection failed
-	return false;
-}
-
-
-
-/**
-* Press event call-back function
-*
-* @param int pin Reports which I/O pin the event occurred on
-* @return void
-*/
-void onPress(int pin)
-{
-	char numberstring[2];
-	sprintf(numberstring, "%d", pin);
-	mqtt.publish("server/button/press", numberstring);
-}
-
-
-
-/**
-* Double press event call-back function
-*
-* @param int pin Reports which I/O pin the event occurred on
-* @return void
-*/
-void onDblPress(int pin)
-{
-	char numberstring[2];
-	sprintf(numberstring, "%d", pin);
-	mqtt.publish("server/button/dblpress", numberstring);
-}
-
-
-
-/**
-* Press-hold event call-back function
-*
-* @param int pin Reports which I/O pin the event occurred on
-* @return void
-*/
-void onHold(int pin)
-{
-	char numberstring[2];
-	sprintf(numberstring, "%d", pin);
-	mqtt.publish("server/button/holding", numberstring);
-}
-
-
-
-/**
- * Process incoming MQTT message
- *
- * @param char* topic The incomnig topic
- * @param byte* payload The incoming message
- * @param int leng Length of payload
- * @return void
- */
-void mqttCallback(char* topic, byte* payload, unsigned int leng)
-{
-}
-
-
-
-/**
- * Reset watch-dog module
- * Prevents Arduino from resetting if everything is fine
- *
- * @return void
- */
-void resetWatchDog()
-{
-	if (getTimer(watchdog_timmer, 1000))
-	{
-		watchdog_status = !watchdog_status;
-		digitalWrite(watchdog_pin, watchdog_status);
-	}
-}
-
-
-
-
-/**
- * Reads byte value from given register
- *
- * @param byte reg The register
- * @return byte The value
- */
-byte readRegister (byte reg)
-{
-	Wire.beginTransmission(MAC_I2C_ADDR);
-	
-	// Register to read
-	Wire.write(reg);
-	Wire.endTransmission();
-
-	// Read a byte
-	Wire.requestFrom(MAC_I2C_ADDR, 1);
-	while(!Wire.available())
-	{
-		// Wait
-	}
-
-	return Wire.read();
-}
+/*------------------------------------------------------------------------*/
 
 
 
@@ -264,20 +40,238 @@ byte readRegister (byte reg)
  */
 bool getTimer (unsigned long &tmr, int interval)
 {
-	// Set initial value
-	if (tmr < 1)
-	{
-		tmr = millis();
-	}
+  // Set initial value
+  if (tmr < 1)
+  {
+    tmr = millis();
+  }
 
-	// Determine difference of our timer against millis()
-	if (millis() - tmr >= interval)
-	{
-		// Complete. Reset timer
-		tmr = 0;
-		return true;
-	}
+  // Determine difference of our timer against millis()
+  if (millis() - tmr >= interval)
+  {
+    // Complete. Reset timer
+    tmr = 0;
+    return true;
+  }
 
-	// Still needs to "count"
-	return false;
+  // Still needs to "count"
+  return false;
+}
+
+/**
+ * Converts IP address to char array
+ *
+ * @param IPAddress address
+ * @return char*
+ */
+char* ip2CharArray(IPAddress address)
+{
+    static char ip[16];
+    sprintf(ip, "%d.%d.%d.%d", address[0], address[1], address[2], address[3]);
+    return ip;
+}
+
+/**
+ * Reconnect to MQTT broker
+ * @return bool True on successful connection
+ * @return bool False on failure to connect
+ */
+bool mqttReconnect()
+{
+  // Watchdog low incase we never reconnect
+  #ifdef ENABLE_WATCHDOG
+    digitalWrite(watchdog_pin, LOW);
+  #endif
+
+  // Too early to reconnect
+  if (!getTimer(mqtt_timer, 1000))
+  {
+    return false;
+  }
+
+  // Connection successful
+  if (mqtt_client.connect(String(mqtt_name + String(random(0xffff), HEX)).c_str(), mqtt_user, mqtt_pass))
+  {
+    // Publish json-string payload of connection status: event type, local IP, controller ID
+    sprintf(mqtt_buffer, "{\"event\":\"reconnected\",\"ip\":\"%s\",\"controller\":\"%d\"}", ip2CharArray(Ethernet.localIP()), controller_id);
+    mqtt_client.publish(mqtt_status_topic, mqtt_buffer);
+
+    return true;
+  }
+
+  // Connection failed
+  return false;
+}
+
+
+
+
+/**
+ * Reset watch-dog module
+ * Prevents Arduino from resetting if everything is fine
+ *
+ * @return void
+ */
+void resetWatchDog()
+{
+  if (getTimer(watchdog_timer, 1000))
+  {
+    watchdog_status = !watchdog_status;
+    digitalWrite(watchdog_pin, watchdog_status);
+  }
+}
+
+
+
+/**
+ * Reads byte value from given register
+ *
+ * @param byte reg The register
+ * @return byte The value at given register
+ */
+byte readRegister (byte reg)
+{
+  Wire.beginTransmission(MAC_I2C_ADDR);
+
+  // Register to read
+  Wire.write(reg);
+  Wire.endTransmission();
+
+  // Read a byte
+  Wire.requestFrom(MAC_I2C_ADDR, 1);
+  while(!Wire.available())
+  {
+    // Wait
+  }
+
+  return Wire.read();
+}
+
+
+
+/**
+ * Determine given button's state and report if needed
+ * 
+ * @param signed char id Button's ID: 0-95
+ * @param byte state Button's state
+ * @return void
+ */
+void updateButton(signed char id, byte state)
+{
+  if (state != button_states[id] && getTimer(button_timers[id], DEBOUNCE_RATE))
+  {
+    button_states[id] = state;
+  }
+  else
+  {
+    return;
+  }
+
+  // Button is pressed
+  if (!state)
+  {
+    // Publish json-string payload of button event: controller ID, button ID
+    sprintf(mqtt_buffer, "{\"controller\":\"%d\",\"button\":\"%d\"}", controller_id, id);
+    mqtt_client.publish(mqtt_publish_topic, mqtt_buffer);
+  }
+}
+
+
+
+/**
+ * Initial setup
+ *
+ * @return void
+ */
+void setup()
+{
+  Wire.begin();
+
+  #ifdef ENABLE_MAC_ADDRESS_ROM
+    ardunio_mac[0] = readRegister(0xFA);
+    ardunio_mac[1] = readRegister(0xFB);
+    ardunio_mac[2] = readRegister(0xFC);
+    ardunio_mac[3] = readRegister(0xFD);
+    ardunio_mac[4] = readRegister(0xFE);
+    ardunio_mac[5] = readRegister(0xFF);
+  #endif
+
+  #ifdef ENABLE_DHCP
+    // Wait for network connection
+    while (Ethernet.begin(ardunio_mac) == 0)
+    {
+      delay(1000);
+    }
+  #else
+    Ethernet.begin(ardunio_mac, ardunio_ip);
+  #endif
+
+  // Initializes the pseudo-random number generator
+  randomSeed(micros());
+
+  // Setup MQTT
+  mqtt_client.setServer(mqtt_broker, 1883);
+
+  // Setup pins
+  signed char id = 0;
+  for (signed char i = 0; i < 6; i++)
+  {
+    mcps[i].begin(i);
+
+    for (signed char p = 0; p < 16; p ++)
+    {
+      mcps[i].pinMode(p, INPUT);
+      mcps[i].pullUp(p, HIGH);
+      id ++;
+    }
+  }
+}
+
+
+/**
+ * Main program loop
+ *
+ * @return void
+ */
+void loop()
+{
+  // Keep network connection
+  Ethernet.maintain();
+
+  // Reconnect to MQTT server
+  if (!mqtt_client.connected())
+  {
+    if (!mqttReconnect())
+    {
+      // Exit early of main loop
+      return;
+    }
+  }
+
+  // Check for incoming MQTT messages
+  mqtt_client.loop();
+
+  // Update each button's state
+  signed char id = 0;
+  for (signed char i = 0; i < 6; i++)
+  {
+    for (signed char p = 0; p < 16; p ++)
+    {
+      updateButton(id, mcps[i].digitalRead(p));
+      id ++;
+    }
+  }
+
+  // Heartbeat report
+  if (getTimer(heart_timer, HEARTBEAT))
+  {
+    // Publish json-string payload of connection status: event type, local IP, controller ID
+    sprintf(mqtt_buffer, "{\"event\":\"status\",\"ip\":\"%s\",\"controller\":\"%d\"}", ip2CharArray(Ethernet.localIP()), controller_id);
+    mqtt_client.publish(mqtt_status_topic, mqtt_buffer);
+  }
+
+  // Prevent arduino from resetting if everything is fine
+  #ifdef ENABLE_WATCHDOG
+    resetWatchDog();
+  #endif
 }
